cfd0221d84c65ba13ed0b2165a87628b01aced1e
Neal Donnelly <NealJMD@gmail.com>
2015-03-03 19:48:11 -0600

    remove forgotten debug code

--- app/controllers/va_ballot_requests_controller.rb
+++ app/controllers/va_ballot_requests_controller.rb
@@ -10,9 +10,4 @@ class VaBallotRequestsController < DocumentController
       params.require(:va_ballot_request).permit(:ssn_four, :reason_code, :reason_support, **address_strong_params)
     end
 
-    def va_ballot_request_params
-      puts "------------------------ someone is calling this..."
-      return params
-    end
-
 end
