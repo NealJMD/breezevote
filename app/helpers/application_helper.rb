21a81bf9b2fac4376ed3d36e6ea177704052544f
Neal Donnelly <NealJMD@gmail.com>
2015-03-05 10:32:06 -0600

    fill out moved recently checkbox on NC br

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,16 @@ module ApplicationHelper
 
   def render_value(value)
     ret = value
+    puts value.class.to_s
     case value.class.to_s
     when "Date"
       ret = value.strftime("<div class='month'>%m</div><div class='slash'>/</div>
                             <div class='day'>%d</div><div class='slash'>/</div>
                             <div class='year'>%Y</div>")
     when "TrueClass"
-      ret = "&times;"
+      ret = "<div class='true positioned'>&times;</div>"
     when "FalseClass"
-      ret = ""
+      ret = "<div class='false positioned'>&times;</div>"
     end
     return ret.to_s.html_safe
   end
