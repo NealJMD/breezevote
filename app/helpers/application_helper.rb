c4724f5479dc566338883310b5c0134aaa55d57b
Neal Donnelly <NealJMD@gmail.com>
2015-03-04 18:50:36 -0600

    create HTML version of NC ballot request form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def render_value(value)
+    ret = value
+    case value.class.to_s
+    when "Date"
+      ret = value.strftime("<div class='month'>%m</div><div class='slash'>/</div>
+                            <div class='day'>%d</div><div class='slash'>/</div>
+                            <div class='year'>%Y</div>")
+    when "TrueClass"
+      ret = "&times;"
+    when "FalseClass"
+      ret = ""
+    end
+    return ret.to_s.html_safe
+  end
+
 end
