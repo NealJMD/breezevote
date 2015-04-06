747ca89d3c27301768fc8a9a8a4676864a3dcd41
Neal Donnelly <NealJMD@gmail.com>
2015-04-06 13:10:56 -0600

    remove unused auto-generated unlock views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 
